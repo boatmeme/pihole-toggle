b0916da7157906c547a08c3a8501afe752ebee05
Jonathan Griggs <jonathan.griggs@gmail.com>
2022-04-17 02:44:18 -0600

    Easy disable / enable implementation

--- app/routes/index.js
+++ app/routes/index.js
@@ -7,8 +7,18 @@ const auth = process.env.PIHOLE_API_KEY || 'nonce'
 
 /* GET home page. */
 router.get('/', async (req, res) => {
-  const stats = await pihole.callApi('summary', { url, auth })
-  res.render('index', { title: 'pi-hole toggler' });
+  const stats = await pihole.callApi('summary', { url, auth });
+  res.render('index', { title: 'pi-hole toggler', disabled: stats.status === 'enabled' ? false : true });
+});
+
+router.post('/toggle_ad_block', async (req, res) => {
+  const stats = await pihole.callApi('summary', { url, auth });
+  if (stats.status === 'enabled') {
+    await pihole.callApi('disable', { url, auth });
+  } else {
+    await pihole.callApi('enable', { url, auth });
+  }
+  res.redirect('/')
 });
 
 module.exports = router;
